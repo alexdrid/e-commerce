f46f61a5ef7540231c7ac5afbbe297dbd74934ed
Alexandros Dridakis <alexandros.dridakis@retailzoom.net>
2022-10-03 10:16:08 +0300

    bug fixes

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ dotenv.config()
 connectDB()
 const app = express()
 
-app.get("/", (req, res) =>
-    res.send('Hello Darkness My Old Friend')
-)
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
+  }
 
 app.use('/api/products', productRoutes)
 
